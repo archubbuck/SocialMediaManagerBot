d23467c4c00563641e86482ab58e5656cd53966f
Adam <Adam.Chubbuck@gmail.com>
2020-11-15 10:01:37 -0500

    .

--- src/index.ts
+++ src/index.ts
@@ -98,7 +98,7 @@ client.on("messageReactionAdd", async (messageReaction: MessageReaction, user: K
                 sr: 'CuteAnimals',
                 kind: 'image',
                 resubmit: true,
-                title: title,
+                title: title + " #animals #cuteanimals #pets",
                 url: imgurUrl
             }).then((response) => {
                 console.log(response);
